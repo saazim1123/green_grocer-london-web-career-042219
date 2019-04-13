6d3114754598bd50283ead602612369bbb96ac89
Adnan Azim <saazim1123@gmail.com>
2019-04-13 13:34:23 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,4 +1,16 @@
-
+def consolidate_cart(cart)
+  hash = {}
+  cart.each do |item_hash|
+    item_hash.each do |name, price_hash|
+      if hash[name].nil?
+        hash[name] = price_hash.merge({:count => 1})
+      else
+        hash[name][:count] += 1
+      end
+    end
+  end
+  hash
+end
 
 =begin
 def consolidate_cart(cart: [])
