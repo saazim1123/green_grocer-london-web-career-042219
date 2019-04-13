6fc07932aad3cfc6edbb47aab56be5b7bdd4c6ee
Adnan Azim <saazim1123@gmail.com>
2019-04-13 14:02:35 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -45,7 +45,12 @@ def apply_coupons(cart, coupons)
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |item, price_hash|
+    if price_hash[:clearance] == true
+      price_hash[:price] = (price_hash[:price] * 0.8).round(2)
+    end
+  end
+  cart
 end
 
 def checkout(cart, coupons)
